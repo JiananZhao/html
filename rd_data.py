ba72bf91cf563a66ad46bbd0c00c07b511504fd6
JiananZhao <47008031+JiananZhao@users.noreply.github.com>
2025-09-27 23:49:32 -0400

    Update rd_data.py

--- rd_data.py
+++ rd_data.py
@@ -1,3 +1,4 @@
+py -m pip install plotly  
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio # <-- Import plotly.io for file writing
@@ -89,4 +90,5 @@ fig.update_layout(
 # Display the figure using the robust method:
 output_file = 'yield_curve_animation.html'
 # You will need 'import plotly.io as pio' at the top of your script
+
 pio.write_html(fig, file=output_file, auto_open=True)
