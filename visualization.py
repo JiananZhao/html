2dd0edb7937881bf435975437405a6fddad55593
JiananZhao <47008031+JiananZhao@users.noreply.github.com>
2025-10-29 14:54:20 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -298,8 +298,7 @@ def create_credit_spread_chart(df_data):
         labels={'x': '日期', 'Option-Adjusted Spread (%)': '利差 (%)'},
     )
     # 范围选择器和滑动条
-    #fig.update_layout
-    fig.update_layout = dict(
+    fig.update_layout(
         xaxis=dict(
             rangeselector=dict(
                 buttons=list([
@@ -312,7 +311,6 @@ def create_credit_spread_chart(df_data):
             rangeslider=dict(visible=True, thickness=0.07),
             range=[df_data.index[-1] - pd.DateOffset(years=1), df_data.index[-1]]
         ),
-
         yaxis=dict(
             title="期权调整利差 (%)",
             autorange=True,  # 确保 Y 轴在 X 轴范围改变时自动重新计算并缩放
@@ -321,7 +319,6 @@ def create_credit_spread_chart(df_data):
         hovermode="x unified",
         height=550,
         template="plotly_white"
-
     )
 
     return fig
