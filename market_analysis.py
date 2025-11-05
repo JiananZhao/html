6b40fcaddf04879019e44ad5a76d76d0023cbff3
JiananZhao <47008031+JiananZhao@users.noreply.github.com>
2025-11-04 21:38:44 -0500

    Update market_analysis.py

--- market_analysis.py
+++ market_analysis.py
@@ -77,7 +77,7 @@ def get_sp500_stock_data():
     FILE_PATH = 'spy500_data.csv'
     TTL_SECONDS = 60*1 # 60 minute Time-To-Live (TTL)
     sp500_symbols = get_sp500_symbols() 
-    
+    st.info(sp500_symbols)
     if not sp500_symbols:
         st.warning("未能获取 S&P 500 成分股列表，无法下载股票数据。")
         return None
